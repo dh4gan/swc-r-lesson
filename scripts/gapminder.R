d2aab77ff634b74b2c13957ecdbb798eb71f623a
Duncan Forgan <dhf3@st-andrews.ac.uk>
2017-12-06 15:38:13 +0000

    Add first plots with ggplot2

--- scripts/gapminder.R
+++ scripts/gapminder.R
@@ -1,8 +1,37 @@
 # Script for analysing gapminder data directly from online source
+# Plotting with ggplot2 (part of the Tidyverse, a collection of packages for data science)
 
 # Load gapminder data directly from URL
 url <-paste("https://raw.githubusercontent.com/resbaz/r-novice-gapminder-files/master/data/gapminder-FiveYearData.csv",
             sep = '')
 
 gapminder <- read.table(url,sep=",", header=TRUE)
+
+library(ggplot2)
+
+# ggplot2 works from the following elements:
+# data
+# aesthetics - how is a data point rendered?
+# geoms - what kind of plot is it? Scatter, line, bar ...
+# layers - How many different data elements are overplotted?
+
+# Compare: basic plot using standard library
+
+# plot life expectancy vs GDP per capita, colour coded by continent
+#plot(gapminder$lifeExp, gapminder$gdpPercap, col = gapminder$continent)
+
+# ggplot2: qplot
+#qplot(lifeExp, gdpPercap, data=gapminder, colour=continent)
+
+# More flexible plotting commands
+
+# GDP vs life expectancy
+p <-ggplot(data=gapminder, aes(x=lifeExp, y=gdpPercap, colour=continent))
+# make this a line plot, with data grouped by country, overlaid with a transparent scatter plot
+p + geom_line(aes(group=country)) + geom_point(alpha=0.4) 
+
+# Life expectancy vs year
+p <-ggplot(data=gapminder, aes(x=year, y=lifeExp, colour=continent))
+#p + geom_point() # make this a scatter plot
+p + geom_line(aes(group=country)) + geom_point(alpha=0.35) # make this a line plot, with data grouped by country
 
