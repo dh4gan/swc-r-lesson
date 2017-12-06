8a3d182c5556311852f5683cbdf306572cb8778c
Duncan Forgan <dhf3@st-andrews.ac.uk>
2017-12-06 16:07:10 +0000

    Add code investigating layers and facets

--- scripts/gapminder.R
+++ scripts/gapminder.R
@@ -27,11 +27,34 @@ library(ggplot2)
 
 # GDP vs life expectancy
 p <-ggplot(data=gapminder, aes(x=lifeExp, y=gdpPercap, colour=continent))
-# make this a line plot, with data grouped by country, overlaid with a transparent scatter plot
-p + geom_line(aes(group=country)) + geom_point(alpha=0.4) 
+
+# Add various layers to plot to modify it
+p + geom_line(aes(group=country)) # line data grouped by country
+p + geom_point(alpha=0.4) # transparent scatter plot on top
+p + scale_y_log10() # y log scale
 
 # Life expectancy vs year
 p <-ggplot(data=gapminder, aes(x=year, y=lifeExp, colour=continent))
 #p + geom_point() # make this a scatter plot
 p + geom_line(aes(group=country)) + geom_point(alpha=0.35) # make this a line plot, with data grouped by country
+
+
+# statistics layers - overplot contours
+p <- ggplot(data=gapminder,aes(x=lifeExp, y=gdpPercap))
+p <- p + geom_point(alpha=0.4, aes(colour=continent)) + scale_y_log10()
+p + geom_density2d()
+
+# Multiple plots
+# Compare life expectancy over time by country, split by continent (using facet_wrap)
+# All x and y axes are consistent according to global extrema
+p <-ggplot(data=gapminder, aes(x=year,y=lifeExp, 
+                               colour=continent, 
+                               group=country))
+p + geom_line() + scale_y_log10() + facet_wrap(~continent)
+
+
+p <- ggplot(data=gapminder, aes(x=pop, y=gdpPercap, colour=continent))
+p <- p + geom_point(alpha=0.2) + scale_x_log10() + scale_y_log10()
+p + geom_density_2d() + facet_wrap(~continent)
+
 
